66d0144fd7f2d20e333325db7eb7c75ab87b72a7
itsmattsaudners <itsmattsaunders@gmail.com>
2020-12-07 12:02:35 +1300

    fixed some errors, typos and stuff

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
 const main = document.getElementById('main')
-const addUserBtn = document.getElementById('add-user')
+const addUserBtn = document.getElementById('add_user')
 const doubleBtn = document.getElementById('double')
-const showMillionaires = document.getElementById('show-millionaires')
+const showMillionaires = document.getElementById('show_millionaires')
 const sort = document.getElementById('sort')
-const calculateBtn = document.getElementById('calculate-wealth')
+const calculateBtn = document.getElementById('calculate_wealth')
 
 let data = [];
 
-getRandomUser();
-getRandomUser();
-getRandomUser();
-
 //fetch random user and add money
 async function getRandomUser(){
+  console.log('click')
   const res = await fetch('https://randomuser.me/api')
   const data = await res.json();
   const user = data.results[0];
@@ -20,6 +17,37 @@ async function getRandomUser(){
     name: `${user.name.first} ${user.name.last}`,
     money: Math.floor(Math.random() * 1000000)
   }
-
-  console.log(newUser)
+  addData(newUser)
 }
+
+
+
+//Add new obj to data arr
+function addData(obj){
+  data.push(obj)
+  updateDOM();
+
+
+// Update DOM
+function updateDOM(providedData = data) {
+  // Clear main div
+  main.innerHTML = '<h2><strong>Person</strong> Wealth</h2>';
+
+  providedData.forEach(item => {
+    const element = document.createElement('div');
+    element.classList.add('person');
+    element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(
+      item.money
+    )}`;
+    main.appendChild(element);
+    });
+  }
+}
+
+//Format number as money
+function formatMoney(number){
+ return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+}
+
+//ecent listeners
+addUserBtn.addEventListener('click', getRandomUser);
